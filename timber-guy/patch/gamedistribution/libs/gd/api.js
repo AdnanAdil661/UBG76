d998c6a28f52aaa2a3af7a743994b7855f4bea8b
AdnanAdil661 <106970172+AdnanAdil661@users.noreply.github.com>
2025-11-05 19:47:59 +0000

    Add batch ending with top-speed-3d

---
+++ timber-guy/patch/gamedistribution/libs/gd/api.js
@@ -0,0 +1,8 @@
+gdApi= new function() {
+    this.play= function() {
+        console.log("gdApi.play");
+    }
+    this.showBanner= function() {
+        console.log("gdApi.showBanner");
+    }
+}
