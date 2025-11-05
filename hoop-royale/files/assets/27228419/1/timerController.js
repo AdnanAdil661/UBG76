61530088ca099874828e6555bfd346a6fda90cac
AdnanAdil661 <106970172+AdnanAdil661@users.noreply.github.com>
2025-11-05 19:39:27 +0000

    Add batch ending with hover-racer-drive

---
+++ hoop-royale/files/assets/27228419/1/timerController.js
@@ -0,0 +1,49 @@
+var TimerController = pc.createScript('timerController');
+
+// initialize code called once per entity
+TimerController.prototype.initialize = function() {
+    var ROUND_TIME = 60,
+        timerContainer = this.entity.findByName('Timer Container'),
+        timer = this.entity.findByName('Game timer'),
+        timerInt,
+        
+        setTime = function (time) {
+            timer.element.text = '00:' + (time < 10 ? '0' : '') + time;
+        }.bind(this),
+        
+        tick = function () {
+            setTime(--this.remainingTime);
+            
+            if (!this.remainingTime)
+                this.app.fire('Round:end', this.roundMode);
+            
+        }.bind(this),
+        
+        startTimer = function () {
+            timerInt = setInterval(tick, 1000);
+        }.bind(this),
+        
+        stopTimer = function () {
+            clearInterval(timerInt);
+        }.bind(this),
+        
+        resetTimer = function () {
+            this.remainingTime = ROUND_TIME;
+            setTime(this.remainingTime);
+        }.bind(this);
+    
+    this.app.on('Round:enter', function (roundMode) {   
+        timerContainer.enabled = (roundMode == Conf.ROUND_MODE);
+        resetTimer();
+        this.roundMode = roundMode;
+    }.bind(this));
+    
+    this.app.on('Round:start', function (roundMode) {
+        if (roundMode == Conf.ROUND_MODE)
+            startTimer();
+    });
+    this.app.on('Round:end', stopTimer);
+    
+    this.app.on('Game:pause', stopTimer);
+    this.app.on('Game:unpause', startTimer);
+};
