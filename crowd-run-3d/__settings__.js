fe5b039705bd4d708628a2ba455b2135148f4ca8
AdnanAdil661 <106970172+AdnanAdil661@users.noreply.github.com>
2025-11-05 19:33:12 +0000

    Add batch ending with doodle-baseball

---
+++ crowd-run-3d/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1385199.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 76766183, 76766185, 76766180, 76766182, 76766188, 76766186, 76766184, 76766172, 76766176, 76766177, 76766189, 76766190, 76766192, 76766194, 76766193, 76766195, 76766196, 76766198, 76766199, 76766202, 76766203, 76766208, 76766211, 76766222, 76766223, 76766225, 76766226, 76766233, 76766232, 76766235, 76766236, 76766237, 76766290, 76766333, 76766337, 76766338, 76766340, 76766341, 76766344, 76766349, 76766399, 76766407, 76766420, 76766421, 76766424, 76766278, 76766429, 76766430, 76766431, 129257208, 129258776 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/76766146/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/76766151/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/76766147/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/126855777/1/draco.wasm.js', 'wasmUrl' : 'files/assets/126855778/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/126855776/1/draco.js', 'preload' : true},
+];
